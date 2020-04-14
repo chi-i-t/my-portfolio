f07e2498ed8ab936145f54d67a8b1177cae6704e
chi-i-t <59216455+chi-i-t@users.noreply.github.com>
2020-04-14 15:55:28 +0900

    profile page view
    プロフィールページのビュー作成。

--- components/header.vue
+++ components/header.vue
@@ -2,11 +2,11 @@
   <header class="header">
     <nav>
       <ul class="nav">
-        <li><router-link to="/" active-class="active-list" exact>TOP</router-link></li>
-        <li><router-link to="/profile" active-class="active-list" exact>PROFILE</router-link></li>
-        <li><router-link to="/portfolio" active-class="active-list" exact>PRTFOLIO</router-link></li>
-        <li><router-link to="/skil" active-class="active-list" exact>SKIL</router-link></li>
-        <li><router-link to="/contact" active-class="active-list" exact>CONTACT</router-link></li>
+        <li><router-link to="/">TOP</router-link></li>
+        <li><router-link to="/profile">PROFILE</router-link></li>
+        <li><router-link to="/portfolio">PRTFOLIO</router-link></li>
+        <li><router-link to="/skil">SKIL</router-link></li>
+        <li><router-link to="/contact">CONTACT</router-link></li>
       </ul>
     </nav>
   </header>
--- pages/profile.vue
+++ pages/profile.vue
@@ -1,8 +1,42 @@
 <template>
-  <div class="container">
-    <div class="title">
-      <h1>PROFILE</h1>
+  <div class="profile-contants">
+    <h1 class="title">PROFILE</h1>
+    <div class="profile-main">
+      <div class="portrait">
+        <img src="~/assets/portrait.jpg" width="300" height="250">
+      </div>
+      <div class="list">
+        <table width="100%" height="200">
+          <tr>
+            <td>Name</td>
+            <td>高橋 千絵</td>
+          </tr>
+          <tr>
+            <td>University</td>
+            <td>成蹊大学 法学部法律学科</td>
+          </tr>
+          <tr>
+            <td>PreviousJob</td>
+            <td>株式会社 みずほ銀行</td>
+          </tr>
+          <tr>
+            <td>Github</td>
+            <td>chi-i-t</td>
+          </tr>
+          <tr>
+            <td>Twitter</td>
+            <td>@chi_i_TECH</td>
+          </tr>
+        </table>
+        <div class="message">
+          前職にて新しいシステムを導入したことにより、ITによる事務効率化に感動。その感動をIT慣れしていないユーザーにも伝えたい。<br>
+          退職後、2019年12月よりテックキャンプにてプログラミングの勉強を開始。<br>
+          2020年３月同スクール卒業。現在はPythonを独学で学ぶ。
+        </div>
+      </div>
     </div>
+    
   </div>
+
 </template>
 
