1f17af4b993099e6b3f14e9902acdcc7aead2d2f
chi-i-t <59216455+chi-i-t@users.noreply.github.com>
2020-04-15 18:39:00 +0900

    contact page view
    仮の問い合わせフォームを作成。
    Netlifyにデプロイし、Netlify Formを使用予定。

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,7 +1,31 @@
 <template>
   <div class="container">
-    <div>
-      <h1>CONTACT</h1>
+    <h2 class="title">CONTACT</h2>
+    <div class="contact-main">
+      <div class="contact-message">
+        ご連絡は以下のフォームからお願い致します。
+      </div>
+      <div class="contact-form"> 
+        <form name="contact" method="POST" data-netlify="true">
+          <p>
+            <label>お名前<input type="text" name="name" /></label>   
+          </p>
+          <p>
+            <label>メールアドレス<input type="email" name="email" /></label>
+          </p>
+          <p>
+            <label>メッセージ<textarea name="message"></textarea></label>
+          </p>
+          <p>
+            <button type="submit">送信</button>
+          </p>
+        </form>
+      </div>   
+      
+
+
+
+
     </div>
   </div>
 </template>
