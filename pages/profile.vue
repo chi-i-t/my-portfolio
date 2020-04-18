3efeb69d9dee66bb733c09d7eb78aeb59b6214f0
chi-i-t <59216455+chi-i-t@users.noreply.github.com>
2020-04-18 15:30:43 +0900

    update profile.css

--- pages/profile.vue
+++ pages/profile.vue
@@ -6,7 +6,7 @@
         <img class="profile-image" src="portrait.jpg">
       </div>
       <div class="profile-list">
-        <table width="100%" height="200">
+        <table width="100%" height="50%">
           <tr>
             <td>Name</td>
             <td>高橋 千絵</td>
